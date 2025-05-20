48ad7a522c0be38c06f7901e16bfa3ef5506f6cf
Gohar Wahidy <goharwah786@gmail.com>
2025-05-19 21:57:00 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="flex items-center justify-between p-2 bg-[#c10008] text-white z-50">
       <div className="flex items-center">
         <img src={Logo} alt="Barber Shop Logo" className="h-10 w-10 mr-2 ml-2" />
-        <h1 className="text-2xl font-bold mr-2">BARBER SHOP</h1>
+        <h1 className="text-2xl font-bold mr-2">© BARBER SHOP</h1>
       </div>
       <a href='tel:+13192902150' className="hover:opacity-80 transition-opacity">
         <img src={Phone} alt="Phone" className="h-10 w-10 ml-2 mr-2" />
