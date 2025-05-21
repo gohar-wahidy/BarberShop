c8227dcf15b8b77143b9b209386e6f63c9b1e5da
Gohar <goharwah786@gmail.com>
2025-05-21 13:40:33 -0500

    New Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Phone from '../assets/phone.png';
 
 function Footer() {
   return (
-    <footer className="flex items-center justify-between p-2 bg-[#c10008] text-white z-50">
+    <footer className="flex items-center justify-between p-2 bg-black text-white z-50">
       <div className="flex items-center">
         <img src={Logo} alt="Barber Shop Logo" className="h-10 w-10 mr-2 ml-2" />
         <h1 className="text-2xl font-bold mr-2">© BARBER SHOP</h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/bslogo.png';
 
 function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 flex items-center justify-center p-2 bg-[#c10008] text-white z-50">
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-center p-2 bg-black text-white z-50">
       <div className="flex items-center">
         <img src={Logo} alt="Barber Shop Logo" className="h-10 w-10 mr-2" />
         <h1 className="text-2xl font-bold">BARBER SHOP</h1>
